81fb3c950ecb7716ec29a42a39e3922a90767b8c
Yogita171 <yogitagaikwad8662gmail.com>
2024-03-21 02:26:46 +0530

    new file

--- Control_Flow/truthy.js
+++ Control_Flow/truthy.js
@@ -4,3 +4,20 @@
 
 //truthy values-
 //"0",'false'," ",[],{},function[]{}
+
+//Nullish Coalescing Operator(??)
+
+let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??15
+val1=null??10??15
+// console.log(val1)
+
+//ternary operator
+
+//syntax
+//condition ? true:false
+
+const iceTeaPrice=100
+iceTeaPrice <=80 ? console.log("less than 80") : console.log("more than 80")
