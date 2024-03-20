1a5d6ab51d54cdd3689a608d3e8fd90d3718696d
Yogita171 <yogitagaikwad8662gmail.com>
2024-03-21 02:06:55 +0530

    new files

---
+++ Control_Flow/one.js
@@ -0,0 +1,52 @@
+//if statement
+
+const isLoggedIn=true
+const temperature=41
+// if(2=="2"){
+//     console.log("executed")
+// }
+
+// if(2!=="2"){
+//     console.log("executed")
+// }
+
+// if(temperature==41){
+//     console.log("temperature is less than 50")
+// }else{
+//     console.log("temperature is more than 50")
+// }
+
+// const score=200
+// if(score>100){
+//     let power="fly"
+//     console.log(`User power:${power}`)
+// }
+// console.log(`User power:${power}`)
+
+//nestedif
+const balance=1000
+// if(balance<500)
+// {
+//     console.log('less than 500');
+// }else if(balance<750){
+//     console.log('less than 750')
+// }else if(balance<900){
+//     console.log('less than 900')
+// }else{
+//     console.log("balance is satisfactory")
+// }
+
+// const userLoggedIn=true
+// const debitCard=true
+
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy courses")
+// }
+
+
+// const loggedInFromGoogle=false
+// const loggedInFromEmail=true
+
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in")
+// }
