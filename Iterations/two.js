6a16fb115caa5bd32ecaf422bfaa04305ddd14e5
Yogita171 <yogitagaikwad8662gmail.com>
2024-03-21 23:12:48 +0530

    while and do while loop

---
+++ Iterations/two.js
@@ -0,0 +1,20 @@
+//while loop
+let index=0
+while (index<=10) {
+    //console.log(`Value of index is ${index}`)
+    index=index+2
+}
+
+let myArray=["chips","kurkure","biscuit","wheels"]
+let arr=0
+while(arr<myArray.length){
+    //console.log(`Value is ${myArray[arr]}`)
+    arr++
+}
+
+//do-while loop
+let score=1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10)
